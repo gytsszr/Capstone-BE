c626b97629b2bcc514b845e763e556cba8d11e4b
Sem28 <samuelzakaria28@gmail.com>
2023-11-22 22:00:50 +0700

    Routes update TBC

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,64 @@ const routes = [
         handler: getUser
     },
     {
+        method: 'GET',
+        path: "/api/users",
+        handler: getUser
+        // TBC
+    },
+    {
         method: 'POST',
-        path: "/addUser",
-        handler: register
+        path: "/api/users/login",
+        handler: getUser
+        // TBC
+    },
+    {
+        method: 'PUT',
+        path: "/api/users/current",
+        handler: getUser
+        // TBC
+    },
+    {
+        method: 'DELETE',
+        path: "/api/users/logout",
+        handler: getUser
+        // TBC
+    },
+    {
+        method: 'POST',
+        path: "/api/users/resume",
+        handler: getUser
+        // TBC
+    },
+    {
+        method: 'PUT',
+        path: "/api/users/resume",
+        handler: getUser
+    },
+    {
+        method: 'GET',
+        path: "/api/users/resume",
+        handler: getUser
+    },
+    {
+        method: 'DELETE',
+        path: "/api/users/resume",
+        handler: getUser
+    },
+    {
+        method: 'POST',
+        path: "/api/applyment",
+        handler: getUser
+    },
+    {
+        method: 'GET',
+        path: "/api/applyment",
+        handler: getUser
+    },
+    {
+        method: 'DELETE',
+        path: "/api/applyment",
+        handler: getUser
     },
     //costumer uncomment code dibawah
     // {
