052b959049b9b45928ed7907e33a314b686c8381
Sem28 <samuelzakaria28@gmail.com>
2023-11-22 09:44:29 +0700

    sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 //routes boleh diisi disini yaa
 const {getUser} = require('./user/handler');
 const routes = [
+    //user
     {
         method: 'GET',
         path: "/",
         handler: getUser
     },
+    //costumer uncomment code dibawah
+    // {
+
+    // },
+    //admin,
+    // {
+
+    // }
 ];
 
 module.exports = routes;
