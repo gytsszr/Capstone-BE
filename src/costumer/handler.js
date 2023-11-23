9c10c4aefd245310ec8195da3e71e194ee037d9a
sita <79690301+arsitanrfzh@users.noreply.github.com>
2023-11-23 10:29:53 +0700

    Update customer
    menambahkan getCustomer (23/11/23)

---
+++ src/costumer/handler.js
@@ -0,0 +1,51 @@
+const { customers } = require('../../models'); // Ganti dengan path yang benar ke file models/users.js
+
+// Ambil data dari SQLite
+const getCustomer = async () => {
+    try {
+        const customersData = await customers.findAll();
+        return customersData;
+    } catch (err) {
+        console.error('Terjadi kesalahan:', err);
+        throw err;
+    }
+};
+
+const register = async (request, h) => {
+    const {
+        firstName, 
+        lastName, 
+        email, 
+        password, 
+        job, 
+        sex, 
+        address,
+        website,
+        description
+    } = request.payload;
+
+    try {
+        // Membuat customer baru dan menyimpannya ke database
+        const newCustomer = await customers.create({
+            firstName, 
+            lastName, 
+            email, 
+            password, 
+            job, 
+            sex, 
+            address,
+            website,
+            description
+        });
+
+        // Mengembalikan data customer yang baru dibuat
+        return newCustomer;
+    } catch (err) {
+        console.error('Terjadi kesalahan:', err);
+        throw err;
+    }
+}
+
+module.exports = {
+    getCustomer, register
+};
