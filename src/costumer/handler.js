28ccc679b6792bd8f302fd49ce69a5e938fa101b
sita <79690301+arsitanrfzh@users.noreply.github.com>
2023-11-23 14:21:52 +0700

    Update customer

--- src/costumer/handler.js
+++ src/costumer/handler.js
@@ -1,52 +1,131 @@
-const { customers } = require('././handler'); // Ganti dengan path yang benar ke file models/users.js
+const { customers } = require('./handler'); // Ganti dengan path yang benar ke file models/users.js
 
 // Ambil data dari SQLite
-const getCustomer = async () => {
-    try {
-        const customersData = await customers.findAll();
-        return customersData;
-    } catch (err) {
-        console.error('Terjadi kesalahan:', err);
-        throw err;
-    }
+const getCustomers = async () => {
+  try {
+    const customers = await User.findAll({
+      where: { isCustomer: true },
+      orderBy: {
+        firstName: "asc",
+      },
+    });
+    return customers;
+  } catch (err) {
+    console.error('Terjadi kesalahan:', err);
+    throw err;
+  }
 };
 
-const register = async (request, h) => {
-    const {
-        firstName, 
-        lastName, 
-        email, 
-        password, 
-        job, 
-        sex, 
-        address,
-        website,
-        description,
-        isCustomer
-    } = request.payload;
+//Mengubah fungsi `register` untuk menerima parameter `isCustomer` secara default
+const register = async (req, h) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    job,
+    sex,
+    address,
+    website,
+    description,
+  } = req.body;
 
-    try {
-        // Membuat customer baru dan menyimpannya ke database
-        const newCustomer = await customers.create({
-            firstName, 
-            lastName, 
-            email, 
-            password, 
-            job, 
-            sex, 
-            address,
-            website,
-            description
-        });
+  try {
+    //Mengubah parameter `isCustomer` menjadi `true` secara default
+    const newCustomer = await User.create({
+      firstName,
+      lastName,
+      email,
+      password,
+      job,
+      sex,
+      address,
+      website,
+      description,
+      isCustomer: true,
+    });
 
-        // Mengembalikan data customer yang baru dibuat
-        return newCustomer;
-    } catch (err) {
-        console.error('Terjadi kesalahan:', err);
-        throw err;
+    //Validasi data
+    if (!email.includes("@")) {
+      return h.status(400).json({ error: "Email tidak valid" });
     }
-}
+
+    if (password.length < 8) {
+      return h.status(400).json({ error: "Password harus lebih dari 8 karakter" });
+    }
+
+
+    //Mengembalikan data customer yang baru dibuat
+    return newCustomer;
+  } catch (err) {
+    console.error('Terjadi kesalahan:', err);
+    throw err;
+  }
+};
+
+// Menambahkan fungsi `getCustomerById` untuk mengambil data customer berdasarkan ID
+const getCustomerById = async (req, h) => {
+  const userId = req.params.id_customer;
+
+  try {
+    const customer = await User.findByPk(userId);
+
+    if (!customer) {
+      return h.status(404).json({ error: "Customer not found" });
+    }
+
+    return customer;
+  } catch (err) {
+    console.error('Terjadi kesalahan:', err);
+    throw err;
+  }
+};
+
+//Menambahkan fungsi `updateCustomer` untuk memperbarui data customer
+const updateCustomer = async (req, h) => {
+  const userId = req.params.id_customer;
+
+  try {
+    const customer = await User.findByPk(userId);
+
+    if (!customer) {
+      return h.status(404).json({ error: "Customer not found" });
+    }
+
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      job,
+      sex,
+      address,
+      website,
+      description,
+    } = req.body;
+
+    customer.firstName = firstName;
+    customer.lastName = lastName;
+    customer.email = email;
+    customer.password = password;
+    customer.job = job;
+    customer.sex = sex;
+    customer.address = address;
+    customer.website = website;
+    customer.description = description;
+
+    await customer.save();
+
+    return customer;
+  } catch (err) {
+    console.error('Terjadi kesalahan:', err);
+    throw err;
+  }
+};
 
 module.exports = {
-    getCustomer, register
+  getCustomers,
+  register,
+  getCustomerById,
+  updateCustomer,
 };
