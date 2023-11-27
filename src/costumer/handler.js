9a0808b3a6c701da8c0808f70ad6183c25c4dc6b
sita <79690301+arsitanrfzh@users.noreply.github.com>
2023-11-27 17:19:38 +0700

    update customer

--- src/costumer/handler.js
+++ src/costumer/handler.js
@@ -1,4 +1,4 @@
-const { customers } = require('./handler'); // Ganti dengan path yang benar ke file models/users.js
+const { customers } = require('./handler');
 const CryptoJS = require('crypto-js');
 
 const encryptData = (data, key) => {
@@ -11,9 +11,9 @@ const decryptData = (ciphertext, key) => {
 };
 
 // Ambil data dari SQLite
-const getCustomers = async () => {
+const getCustomer = async () => {
   try {
-    const customers = await User.findAll({
+    const customers = await users.findAll({
       where: { isCustomer: true },
       orderBy: {
         firstName: "asc",
@@ -27,7 +27,7 @@ const getCustomers = async () => {
 };
 
 //Mengubah fungsi `register` untuk menerima parameter `isCustomer` secara default
-const registerCustomers = async (req, h) => {
+const registerCustomer = async (req, h) => {
   const {
     firstName,
     lastName,
@@ -42,7 +42,7 @@ const registerCustomers = async (req, h) => {
 
   try {
     //Mengubah parameter `isCustomer` menjadi `true` secara default
-    const newCustomer = await User.create({
+    const newCustomer = await users.create({
       firstName,
       lastName,
       email,
@@ -64,7 +64,6 @@ const registerCustomers = async (req, h) => {
       return h.status(400).json({ error: "Password harus lebih dari 8 karakter" });
     }
 
-
     //Mengembalikan data customer yang baru dibuat
     return newCustomer;
   } catch (err) {
@@ -73,12 +72,58 @@ const registerCustomers = async (req, h) => {
   }
 };
 
+const loginCustomers = async (request, h) => {
+    const {
+        email, 
+        password,
+    } = request.payload;
+
+    try {
+        // Mencari user di database berdasarkan email dan password
+        const customer = await users.findOne({
+            where: {
+                email: email,
+                password: password,
+            }
+        });
+
+        if (!customer) {
+            return h.response({ message: 'Validation Error' }).code(400);
+        }
+
+        // Mengenkripsi data user
+        const key = 'Jobsterific102723'; // Ganti dengan kunci rahasia Anda
+
+        const encryptedData = encryptData({
+            email: customer.email,
+            password: customer.password,
+            firstName: customer.firstName
+        }, key);
+
+        // Menyimpan token ke database
+        customer.token = encryptedData;
+        await customer.save();
+
+        // Mengembalikan data terenkripsi
+        return h.response({ message: `Success Login`, customer}).code(200);
+
+    } catch (err) {
+        console.error('Terjadi kesalahan:', err);
+        return h.response({ message: 'Validation Error' }).code(400);
+    }
+}
+
 // Menambahkan fungsi `getCustomerById` untuk mengambil data customer berdasarkan ID
 const getCustomerById = async (req, h) => {
+  const token = request.headers['token'];
   const userId = req.params.id_customer;
 
   try {
-    const customer = await User.findByPk(userId);
+    // Mendekripsi token untuk mendapatkan data pengguna
+    const key = 'Jobsterific102723'; // Ganti dengan kunci rahasia Anda
+    const customerData = decryptData(token, key);
+    // Mencari user di database berdasarkan email dan password
+    const customer = await users.findByPk(userId);
 
     if (!customer) {
       return h.status(404).json({ error: "Customer not found" });
@@ -91,12 +136,13 @@ const getCustomerById = async (req, h) => {
   }
 };
 
+
 //Menambahkan fungsi `updateCustomer` untuk memperbarui data customer
 const updateCustomer = async (req, h) => {
   const userId = req.params.id_customer;
 
   try {
-    const customer = await User.findByPk(userId);
+    const customer = await users.findByPk(userId);
 
     if (!customer) {
       return h.status(404).json({ error: "Customer not found" });
@@ -133,15 +179,57 @@ const updateCustomer = async (req, h) => {
   }
 };
 
-  const customerLogout = (req, h) => {
-  req.logout();
-  return h.response({ message: "Logout successful" }, 200);
+// Menambahkan fungsi `getResume` untuk mengambil resume dari user
+const getResume = async (req, h) => {
+  const id_user = req.params.id_user;
+
+  try {
+    const user = await User.findOne({ where: { id: id_user } });
+
+    if (!user) {
+      return h.status(404).json({ error: "User tidak ditemukan" });
+    }
+
+    const resume = user.resume;
+
+    return h.response(resume, 200);
+  } catch (err) {
+    console.error('Terjadi kesalahan:', err);
+    throw err;
+  }
 };
 
+  const customerLogout = (req, h) => {
+  const token = request.headers['token'];
+
+    try {
+        const customer = await users.findOne({
+            where: {
+                token : token
+            }
+        });
+
+        if (!customer) {
+            return h.response({ message: 'Validation Error' }).code(400);
+        }
+
+        // Menghapus token dari database
+        customer.token = null;
+        await customer.save();
+
+        return h.response({ message: 'Success LogOut' }).code(200);
+    } catch (err) {
+        console.error('Terjadi kesalahan:', err);
+        return h.response({ message: 'Validation Error', err}).code(400);
+    }
+}
+
 module.exports = {
-  getCustomers,
-  registerCustomers,
+  getCustomer,
+  registerCustomer,
+  loginCustomer,
   getCustomerById,
   updateCustomer,
+  getResume,
   customerLogout,
 };
