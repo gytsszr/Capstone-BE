e056eeb300381d058c6b1bb2b99a9b57cde84639
sita <79690301+arsitanrfzh@users.noreply.github.com>
2023-11-28 17:40:51 +0700

    update customer routes

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 //routes boleh diisi disini yaa
-const {getUser, createUser, loginUser, getCurrentUser, logoutUser} = require('./user/handler');
+
 const {
   getCustomer,
   registerCustomer,
@@ -10,158 +10,78 @@ const {
   applyBatch,
   getBatches,
   getBatch,
+  updateApplyment,
+  deleteApplyment,
   customerLogout,
 } = require("./customer/handler");
-const {
-  registerAdmin,
-  loginAdmin,
-  getCustomers,
-  registerBatch,
-  getApplyments,
-  getApplyment,
-  logoutAdmin,
-} = require("./admin/handler");
 
 const routes = [
-    //user
-    {
-        method: 'GET',
-        path: '/',
-        handler: getUser
-    },
-    {
-        // CreateNew User API
-        method: 'POST',
-        path: "/api/users",
-        handler: createUser
-    },
-    {
-        // Login
-        method: 'POST',
-        path: "/api/users/login",
-        handler: loginUser
-    },
-    {
-        //Get User
-        method: 'GET',
-        path: "/api/users",
-        handler: getCurrentUser
-    },
-    {
-        method: 'PUT',
-        path: "/api/users/current",
-        handler: getCurrentUser
-        // TBC
-    },
-    {
-        // Logout User
-        method: 'DELETE',
-        path: "/api/users/logout",
-        handler: logoutUser
-    },
-    {
-        method: 'POST',
-        path: "/api/users/resume",
-        handler: getUser
-        // TBC
-    },
-    {
-        method: 'PUT',
-        path: "/api/users/resume",
-        handler: getUser
-    },
-    {
-        method: 'GET',
-        path: "/api/users/resume",
-        handler: getUser
-    },
-    {
-        method: 'DELETE',
-        path: "/api/users/resume",
-        handler: getUser
-    },
-    {
-        method: 'POST',
-        path: "/api/applyment",
-        handler: getUser
-    },
-    {
-        method: 'GET',
-        path: "/api/applyment",
-        handler: getUser
-    },
-    {
-        method: 'DELETE',
-        path: "/api/applyment",
-        handler: getUser
-    },
     //costumer
     {
         method: "GET",
-        path: "/customers",
-        handler: getCustomers,
+        path: "/api/customers",
+        handler: getCustomer,
     },
     {
         method: "POST",
-        path: "/customers/register",
-        handler: register,
+        path: "/api/customers/register",
+        handler: registerCustomer,
+    },
+    {
+        method: "POST",
+        path: "/api/customers/login",
+        handler: loginCustomer,
     },
     {
         method: "GET",
-        path: "/customers/:id_customer",
+        path: "/api/customers/:id_customer",
         handler: getCustomerById,
     },
     {
         method: "PUT",
-        path: "/customers/:id_customer",
+        path: "/api/customers/:id_customer",
         handler: updateCustomer,
     },
     {
         method: "GET",
-        path: "/customers/:id_customer",
+        path: "/api/customers/:id_customer/resume",
         handler: getResume,
     },
     {
         method: "POST",
-        path: "/customers/logout",
+        path: "/api/customers/:id_customer/batches/:batchId/apply",
+        handler: applyBatch,
+    },
+    {
+        method: "GET",
+        path: "/api/customers/:id_customer/resume",
+        handler: getResume,
+    },
+    {
+        method: "GET",
+        path: "/api/customers/batches",
+        handler: getBatches,
+    },
+    {
+        method: "GET",
+        path: "/api/customers/batches/:batchId",
+        handler: getBatch,
+    },
+    {
+        method: "PUT",
+        path: "/api/customers/:id_customer/applyments/:applyment_id",
+        handler: updateApplyment,
+    },
+    {
+        method: "DELETE",
+        path: "/api/customers/:id_customer/applyments/:applyment_id",
+        handler: deleteApplyment,
+    },
+    {
+        method: "POST",
+        path: "/api/customers/logout",
         handler: customerLogout,
     },
-    //admin,
-    {
-        method: "POST",
-        path: "/api/admins",
-        handler: registerAdmin,
-    },
-    {
-        method: "POST",
-        path: "/api/admins/login",
-        handler: loginAdmin,
-    },
-    {
-        method: "GET",
-        path: "/api/admins/customers",
-        handler: getCustomers,
-     },
-     {
-        method: "POST",
-        path: "/api/admins/batches",
-        handler: registerBatch,
-     },
-     {
-        method: "GET",
-        path: "/api/admins/applyments",
-        handler: getApplyments,
-      },
-      {
-        method: "GET",
-        path: "/api/admins/applyments/:applyId",
-        handler: getApplyment,
-      },
-      {
-        method: "POST",
-        path: "/api/admins/logout",
-        handler: logoutAdmin,
-      },
 ];
-
-module.exports = routes;
+module.exports = router;
+    
